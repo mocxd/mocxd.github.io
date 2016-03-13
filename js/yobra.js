6f6d7a90c29b1af90a0dd2fbcea53880958fd7e2
mocxd <mark.adkins.hastings@gmail.com>
2016-03-12 22:05:13 -0500

    rolling in page changes

---
+++ js/yobra.js
@@ -0,0 +1,34 @@
+$(function() {
+    var moniker = 'mocxd'; // i am internet
+
+    // start page ui
+    $( '.menu-icon' ).click(function() {
+        $(this).toggleClass('clicked');
+    });
+
+    $( '.trigger' ).height($(document).height());
+    // end page ui
+
+    // class moniker throughout document
+    $( '*:not(title)' ).each(function() {
+        'use strict';
+        if ( $(this)
+            .clone()
+            .children()
+            .remove()
+            .end()
+            .text().includes(moniker) ) {
+                var html = $(this).html();
+                console.log('Classy Moniker in a', $(this).prop('tagName'));
+                if (html === moniker) {
+                    $(this).html('<span class="moniker">' + moniker + '</span>');
+                } else {
+                    var fragments = html.split(moniker);
+                    for (let f in fragments) {
+                        fragments[f] += (f == fragments.length-1) ? '' : '<span class="moniker">' + moniker + '</span>';
+                    }
+                    $(this).html(fragments.join(''));
+                }
+        }
+    });
+});
